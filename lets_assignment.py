6ed1ba15feff248379fb2652fad792a4f59b4f8f
Mahesh Birajdar <98763822+MaheshBBirajdar@users.noreply.github.com>
2022-04-01 16:26:08 +0530

    Assignment (30/03/2022)
     Creating one task with the help of python function by using default arguments

---
+++ lets_assignment.py
@@ -0,0 +1,7 @@
+# lets Assignment 
+#function with default arguments
+
+def fun(name,full_name):
+    print(name,full_name)
+    
+fun("What is your name:", "LetsUpgrade Full-Stack Batch Assignment")
